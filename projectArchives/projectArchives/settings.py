764c49ac7512692e347e45d311b08dea34097e69
raphael08 <rsiphael@gmail.com>
2023-03-14 18:44:36 +0300

    requirements

--- projectArchives/projectArchives/settings.py
+++ projectArchives/projectArchives/settings.py
@@ -16,10 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-import environ
 
-env = environ.Env()
-environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
